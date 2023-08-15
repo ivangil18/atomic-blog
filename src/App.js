46f1988a5fb6c1940c3eecc333603c82aab2dd9b
Ivan Gil <igil.dev@gmail.com>
2023-08-15 01:23:09 -0400

    DarkMode component created and utilized

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { PostProvider, PostContext } from "./PostContext";
 import { faker } from "@faker-js/faker";
+import DarkMode from "./DarkMode";
 
 function createRandomPost() {
   return {
@@ -10,23 +11,9 @@ function createRandomPost() {
 }
 
 function App() {
-  const [isFakeDark, setIsFakeDark] = useState(false);
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-  useEffect(
-    function () {
-      document.documentElement.classList.toggle("fake-dark-mode");
-    },
-    [isFakeDark]
-  );
-
   return (
     <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
+      <DarkMode />
       <PostProvider>
         <Header />
         <Main />
